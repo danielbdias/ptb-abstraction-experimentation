1df564c0f09f80a13fa6f633ac809e928db8cd29
Daniel Dias <danielbpdias@gmail.com>
2024-07-01 09:21:50 -0300

    update experiments for hvac and uav

--- experiments/statefluentablation/_domains.py
+++ experiments/statefluentablation/_domains.py
@@ -10,9 +10,9 @@ class DomainExperiment:
     policy_hyperparams:        dict
 
 jax_seeds = [
-    42
-    # 42, 101, 967, 103, 61, 
-    # 107, 647, 109, 347, 113, 
+    # 42
+    42, 101, 967, 103, 61, 
+    107, 647, 109, 347, 113, 
     # 139, 127, 367, 131, 13, 137, 971, 139, 31, 149
 ]
 
@@ -20,7 +20,6 @@ domains = [
     DomainExperiment(
         name='HVAC',
         instance='instance1',
-        # action_bounds={'fan-in': (0.05001, None), 'heat-input': (0.0, None)},
         action_bounds={},
         state_fluents=['occupied', 'temp-heater', 'temp-zone'],
         policy_hyperparams=None
@@ -28,18 +27,17 @@ domains = [
     DomainExperiment(
         name='UAV',
         instance='instance1',
-        # action_bounds={'set-acc': (-1, 1), 'set-phi': (-1, 1), 'set-theta': (-1, 1)},
         action_bounds={},
         state_fluents=['phi', 'pos-x', 'pos-y', 'pos-z', 'psi', 'theta', 'vel'],
         policy_hyperparams=None
     ),
-    DomainExperiment(
-        name='MarsRover',
-        instance='instance0',
-        action_bounds={'power-x': (-0.0999, 0.0999), 'power-y': (-0.0999, 0.0999)},
-        state_fluents=['mineral-harvested', 'pos-x', 'pos-y', 'vel-x', 'vel-y'],
-        policy_hyperparams={'harvest': 5.0}
-    )
+    # DomainExperiment(
+    #     name='MarsRover',
+    #     instance='instance0',
+    #     action_bounds={'power-x': (-0.0999, 0.0999), 'power-y': (-0.0999, 0.0999)},
+    #     state_fluents=['mineral-harvested', 'pos-x', 'pos-y', 'vel-x', 'vel-y'],
+    #     policy_hyperparams={'harvest': 5.0}
+    # )
 ]
 
 silent = True
