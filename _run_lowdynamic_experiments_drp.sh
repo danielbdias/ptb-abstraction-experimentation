516b6181c3c5c38117f8f70fcc4f8f838ad3776d
Daniel Dias <danielbpdias@gmail.com>
2024-07-31 21:04:41 -0300

    adding more experiment results

--- _run_lowdynamic_experiments_drp.sh
+++ _run_lowdynamic_experiments_drp.sh
@@ -1,5 +1,5 @@
-PYTHONWARNINGS=ignore python ./experiments/lowdynamicfluent-drp/step0_create_baselise_run_jaxplan.py
+PYTHONWARNINGS=ignore python ./experiments/lowdynamicfluent-drp/step0_create_baselise_run_drp.py
 PYTHONWARNINGS=ignore python ./experiments/lowdynamicfluent-drp/step1_evaluate_fluents.py
 PYTHONWARNINGS=ignore python ./experiments/lowdynamicfluent-drp/step2_create_grounded_ablated_model.py
-PYTHONWARNINGS=ignore python ./experiments/lowdynamicfluent-drp/step3_create_warm_start_policies_and_run_jaxplan.py
+PYTHONWARNINGS=ignore python ./experiments/lowdynamicfluent-drp/step3_create_warm_start_policies_and_run_drp.py
 PYTHONWARNINGS=ignore python ./experiments/lowdynamicfluent-drp/step4_compute_experiment_statistics.py
