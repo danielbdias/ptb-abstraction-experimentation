29a1abcd5fc4255e52aa5062e5a8071f53e83209
Daniel Dias <danielbpdias@gmail.com>
2024-11-12 08:06:54 -0300

    wip

--- _run_intervalanalysis_experiments_slp_step1.sh
+++ _run_intervalanalysis_experiments_slp_step1.sh
@@ -2,6 +2,6 @@
 TIMEFORMAT='Elapsed time is %R seconds to run baseline and find fluents to ablate with SLP.'
 
 time {
-    # PYTHONWARNINGS=ignore python ./experiments/intervalanalysis/step0_create_baselise_run.py
+    PYTHONWARNINGS=ignore python ./experiments/intervalanalysis/step0_create_baselise_run.py
     PYTHONWARNINGS=ignore python ./experiments/intervalanalysis/step1_evaluate_fluents.py
 }
