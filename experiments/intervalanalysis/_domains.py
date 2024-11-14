96d1be36b6c42d2517d592d4226e6eb098bfa3aa
Daniel Dias <danielbpdias@gmail.com>
2024-11-14 08:30:21 -0300

    update experiments

--- experiments/intervalanalysis/_domains.py
+++ experiments/intervalanalysis/_domains.py
@@ -29,14 +29,14 @@ bound_strategies = {
     'percentiles': (IntervalAnalysisStrategy.PERCENTILE, { 'percentiles': [0.05, 0.95] }),
 }
 
-train_seconds = 300
+train_seconds = 600
 
 hvac_experiments = [
     DomainExperiment(
         name                     = 'HVAC',
         instance                 = 'instance_h_100',
         state_fluents            = [ 'temp-zone', 'temp-heater', 'occupied' ],
-        ground_fluents_to_freeze = set([ 'occupied___z1', 'occupied___z2', 'occupied___z3', 'occupied___z4', 'occupied___z5' ]),
+        ground_fluents_to_freeze = set([ 'occupied___z2', 'occupied___z3', 'occupied___z4', 'temp-zone___z2' ]),
         bound_strategies         = bound_strategies,
         experiment_params=PlannerParameters(
             epsilon_error          = None,
@@ -63,37 +63,37 @@ hvac_experiments = [
             )
         )
     ),
-    DomainExperiment(
-        name                     = 'HVAC',
-        instance                 = 'instance_h_20',
-        state_fluents            = [ 'temp-zone', 'temp-heater', 'occupied' ],
-        ground_fluents_to_freeze = set([ 'occupied___z1', 'occupied___z2', 'occupied___z3', 'occupied___z4', 'occupied___z5' ]),
-        bound_strategies         = bound_strategies,
-        experiment_params=PlannerParameters(
-            epsilon_error          = None,
-            epsilon_iteration_stop = None,
-            model_params=PlanningModelParameters(
-                logic=FuzzyLogic(
-                    tnorm  = ProductTNorm(),
-                    weight = 10
-                )
-            ),
-            optimizer_params=OptimizerParameters(
-                plan             = None,
-                optimizer        = optax.rmsprop,
-                learning_rate    = 0.01,
-                batch_size_train = 32,
-                batch_size_test  = 32,
-                action_bounds    = None,
-            ),
-            training_params=TrainingParameters(
-                seed               = 42,
-                epochs             = 30000,
-                train_seconds      = train_seconds,
-                policy_hyperparams = None
-            )
-        )
-    ),
+    # DomainExperiment(
+    #     name                     = 'HVAC',
+    #     instance                 = 'instance_h_20',
+    #     state_fluents            = [ 'temp-zone', 'temp-heater', 'occupied' ],
+    #     ground_fluents_to_freeze = set([ 'occupied___z1', 'occupied___z2', 'occupied___z3', 'occupied___z4', 'occupied___z5' ]),
+    #     bound_strategies         = bound_strategies,
+    #     experiment_params=PlannerParameters(
+    #         epsilon_error          = None,
+    #         epsilon_iteration_stop = None,
+    #         model_params=PlanningModelParameters(
+    #             logic=FuzzyLogic(
+    #                 tnorm  = ProductTNorm(),
+    #                 weight = 10
+    #             )
+    #         ),
+    #         optimizer_params=OptimizerParameters(
+    #             plan             = None,
+    #             optimizer        = optax.rmsprop,
+    #             learning_rate    = 0.01,
+    #             batch_size_train = 32,
+    #             batch_size_test  = 32,
+    #             action_bounds    = None,
+    #         ),
+    #         training_params=TrainingParameters(
+    #             seed               = 42,
+    #             epochs             = 30000,
+    #             train_seconds      = train_seconds,
+    #             policy_hyperparams = None
+    #         )
+    #     )
+    # ),
 ]
 
 powergen_experiments = [
@@ -132,7 +132,7 @@ powergen_experiments = [
         name                     = 'PowerGen',
         instance                 = 'instance_h_20',
         state_fluents            = [ 'prevProd', 'prevOn', 'temperature' ],
-        ground_fluents_to_freeze = set([ 'prevOn___p1', 'prevOn___p2', 'prevOn___p3', 'prevOn___p4', 'prevOn___p5' ]),
+        ground_fluents_to_freeze = set([ 'prevOn___p1', 'prevOn___p2', 'prevOn___p3', 'prevOn___p4', 'prevOn___p5', 'temperature' ]),
         bound_strategies         = bound_strategies,
         experiment_params=PlannerParameters(
             epsilon_error          = None,
