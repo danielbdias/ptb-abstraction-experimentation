381e3ae7294f78d7344db95a78d3e81dfca947ba
Daniel Dias <danielbpdias@gmail.com>
2025-02-19 21:42:41 -0300

    update gurobi run

--- experiments/intervalanalysis-gurobi/_experiment.py
+++ experiments/intervalanalysis-gurobi/_experiment.py
@@ -1,61 +1,31 @@
-import jax
 import optax
 import os
 import time
 
 from dataclasses import dataclass
-from typing import List, Set
+from typing import Set
 from multiprocessing import get_context, freeze_support
 
 from pyRDDLGym.core.compiler.model import RDDLPlanningModel
-from pyRDDLGym_jax.core.logic import ProductTNorm, FuzzyLogic, SigmoidComparison, SoftRounding, SoftControlFlow
-from pyRDDLGym_jax.core.planner import JaxBackpropPlanner, JaxPlan, JaxPlannerStoppingRule
+from pyRDDLGym_gurobi.core.compiler import GurobiRDDLCompiler
+from pyRDDLGym_gurobi.core.planner import GurobiStraightLinePlan
 
 from _config_multiprocess import pool_context, num_workers, timeout
 
-@dataclass(frozen=True)
-class PlanningModelParameters:
-    logic:          FuzzyLogic
+import gurobipy
+from gurobipy import GRB
 
-@dataclass(frozen=False)
-class OptimizerParameters:
-    plan:             JaxPlan
-    optimizer:        optax.GradientTransformation
-    learning_rate:    float
-    batch_size_train: int
-    batch_size_test:  int
-    action_bounds:    dict
-    guess:            dict
+from typing import Any
 
-@dataclass(frozen=False)
-class TrainingParameters:
-    epochs:             int
-    seed:               jax.random.PRNGKey
-    train_seconds:      int
-    policy_hyperparams: dict
-    stopping_rule:      JaxPlannerStoppingRule
-
-@dataclass(frozen=False)
-class PlannerParameters:
-    model_params:               PlanningModelParameters
-    optimizer_params:           OptimizerParameters   
-    training_params:            TrainingParameters
-    topology:                   List[int] = None
+UNBOUNDED = (-GRB.INFINITY, +GRB.INFINITY)
     
-    def is_drp(self):
-        return self.topology is not None
-    
-    def is_slp(self):
-        return not self.is_drp()
-
 @dataclass(frozen=True)
 class DomainInstanceExperiment:
     domain_name:                         str
     instance_name:                       str
     ground_fluents_to_freeze:            Set[str]
     bound_strategies:                    dict
-    drp_experiment_params:               PlannerParameters
-    slp_experiment_params:               PlannerParameters
+    solver_timeout:                      int
     
     def __post_init__(self):
         if self.drp_experiment_params.topology is None:
@@ -71,117 +41,58 @@ class DomainInstanceExperiment:
         
         return domain_path, domain_file_path, instance_file_path
 
-def get_planner_parameters(model_weight : int, learning_rate : float, batch_size : int, epochs : int, policy_hyperparams: dict = None, topology : List[int] = None):
-    return PlannerParameters(
-        topology = topology,
-        model_params = PlanningModelParameters(
-            logic=FuzzyLogic(
-                tnorm      = ProductTNorm(),
-                comparison = SigmoidComparison(weight=model_weight),
-                rounding   = SoftRounding(weight=model_weight),
-                control    = SoftControlFlow(weight=model_weight),
-            )
-        ),
-        optimizer_params = OptimizerParameters(
-            plan             = None,
-            optimizer        = optax.rmsprop,
-            learning_rate    = learning_rate,
-            batch_size_train = batch_size,
-            batch_size_test  = batch_size,
-            action_bounds    = None,
-            guess            = None
-        ),
-        training_params = TrainingParameters(
-            seed               = 42,
-            epochs             = epochs,
-            train_seconds      = 3_600,
-            policy_hyperparams = policy_hyperparams,
-            stopping_rule      = None
-        )
-    )
-
-@dataclass(frozen=True)
-class ExperimentStatistics:
-    iteration:                   int
-    train_return:                float
-    test_return:                 float
-    best_return:                 float
-
-    @staticmethod
-    def from_callback(planner_callback):
-        iteration = planner_callback['iteration']
-
-        # possible keys: 'iteration', 'train_return', 'test_return', 'best_return', 'params', 'best_params', 'last_iteration_improved', 'grad', 'updates', 'action', 'error', 'invariant', 'precondition', 'pvar', 'reward', 'termination'
-        return ExperimentStatistics(
-            iteration=iteration,
-            train_return=planner_callback['train_return'],
-            test_return=planner_callback['test_return'],
-            best_return=planner_callback['best_return'],
-        )
-    
-    def __str__(self) -> str:
-        return 'step={} train_return={:.6f} test_return={:.6f} best_return={:.6f}'.format(
-          str(self.iteration).rjust(4), self.train_return, self.test_return, self.best_return)
-
 @dataclass(frozen=True)
 class ExperimentStatisticsSummary:
-    final_policy_weights:        dict
-    statistics_history:          list
-    elapsed_time:                float
-    last_iteration_improved:     int
+    final_solution:      dict
+    accumulated_reward:  float
+    elapsed_time:        float
+    params:              Any
 
-def run_jax_planner(name : str, rddl_model : RDDLPlanningModel, planner_parameters : PlannerParameters, silent : bool = True):
+
+def run_gurobi_planner(name : str, rddl_model : RDDLPlanningModel, silent : bool = True):
     print(f'[{os.getpid()}] Run: {name} - Status: Starting')
-    
-    if not silent:
-        print(f'[{os.getpid()}] Run: {name} - Params: {planner_parameters}')
     
     start_time = time.time()
 
-    # initialize the planner
-    planner = JaxBackpropPlanner(
-        rddl_model,
-        batch_size_train = planner_parameters.optimizer_params.batch_size_train,
-        batch_size_test  = planner_parameters.optimizer_params.batch_size_test,
-        plan             = planner_parameters.optimizer_params.plan,
-        optimizer        = planner_parameters.optimizer_params.optimizer,
-        optimizer_kwargs = {'learning_rate': planner_parameters.optimizer_params.learning_rate},
-        action_bounds    = planner_parameters.optimizer_params.action_bounds)
+    # initialize the planner and compiler
+    planner = GurobiStraightLinePlan(rddl_model)
+    compiler = GurobiRDDLCompiler(rddl=rddl_model, plan=planner, rollout_horizon=rddl_model.horizon)
+    gurobi_environment = gurobipy.Env()
+
+    # try to use the preconditions to produce narrow action bounds
+    action_bounds = planner.action_bounds.copy()
+    for name in compiler.rddl.action_fluents:
+        if name not in action_bounds:
+            action_bounds[name] = compiler.bounds.get(name, UNBOUNDED)
+
+    planner.action_bounds = action_bounds
 
     # run the planner as an optimization process
-    planner_callbacks = planner.optimize_generator(
-        planner_parameters.training_params.seed, 
-        epochs                 = planner_parameters.training_params.epochs, 
-        policy_hyperparams     = planner_parameters.training_params.policy_hyperparams,
-        train_seconds          = planner_parameters.training_params.train_seconds,
-        guess                  = planner_parameters.optimizer_params.guess,
-        print_summary          = not silent,
-        print_progress         = not silent,
-        stopping_rule          = planner_parameters.training_params.stopping_rule
-    )
-
-    final_policy_weights = None
-    last_iteration_improved = None
-    last_status = None
-    statistics_history = []
-
-    for callback in planner_callbacks:
-        final_policy_weights = callback['best_params']
-        last_iteration_improved = callback['last_iteration_improved']
-        last_status = callback['status']
-
-        statistics = ExperimentStatistics.from_callback(callback)
-        statistics_history.append(statistics)
-
-        if not silent:
-            print(statistics)
-
+    model, _, params = compiler.compile(env=gurobi_environment)
+    model.optimize()
+    
+    print(model)
+    
+    solved = model.SolCount > 0 # check for existence of valid solution
+    
+    if not solved:
+        raise ValueError("Gurobi failed to find a feasible solution")
+    
     end_time = time.time()
     elapsed_time = end_time - start_time
 
-    print(f'[{os.getpid()}] Run: {name} - Status: {last_status} - Elapsed time: {elapsed_time:.2f} seconds')
+    if not silent:
+        print(f'[{os.getpid()}] Run: {name} - Params: {params}')
 
-    return ExperimentStatisticsSummary(final_policy_weights, statistics_history, elapsed_time, last_iteration_improved)
+    print(f'[{os.getpid()}] Run: {name} - Elapsed time: {elapsed_time:.2f} seconds')
+
+    final_solution = {}
+    for var in model.getVars():
+        final_solution[var.varName] = var.X
+
+    accumulated_reward = model.ObjVal
+
+    return ExperimentStatisticsSummary(final_solution, accumulated_reward, elapsed_time, params)
 
 def prepare_parallel_experiment_on_main():
     freeze_support()
--- experiments/intervalanalysis-gurobi/step2_create_baselise_run.py
+++ experiments/intervalanalysis-gurobi/step2_create_baselise_run.py
@@ -5,10 +5,8 @@ import jax
 import pyRDDLGym
 from pyRDDLGym.core.grounder import RDDLGrounder
 
-from pyRDDLGym_jax.core.planner import JaxStraightLinePlan, JaxDeepReactivePolicy
-
-from _config import experiments, jax_seeds, silent, run_drp, run_slp
-from _experiment import run_experiment_in_parallel, prepare_parallel_experiment_on_main, run_jax_planner
+from _config import experiments, jax_seeds, silent, run_gurobi_planner
+from _experiment import run_experiment_in_parallel, prepare_parallel_experiment_on_main
 from _fileio import save_pickle_data
 
 root_folder = os.path.dirname(__file__)
@@ -28,24 +26,14 @@ def perform_experiment(domain_instance_experiment, planner_type, experiment_para
     regular_env_experiment_stats = []
 
     regular_experiment_name = f"{domain_instance_experiment.domain_name} (regular) - {planner_type}"
-
-    for jax_seed in jax_seeds:
-        experiment_params = experiment_params_builder(domain_instance_experiment)
-        experiment_params.training_params.seed = jax.random.PRNGKey(jax_seed)
-        
-        experiment_summary = run_jax_planner(regular_experiment_name, rddl_model=grounded_model, planner_parameters=experiment_params, silent=silent)
-        regular_env_experiment_stats.append(experiment_summary)
+    
+    experiment_summary = run_gurobi_planner(regular_experiment_name, rddl_model=grounded_model, silent=silent)
+    regular_env_experiment_stats.append(experiment_summary)
 
     save_pickle_data(regular_env_experiment_stats, f'{root_folder}/_results/baseline_{planner_type}_run_data_{domain_instance_experiment.domain_name}_{domain_instance_experiment.instance_name}.pickle')
 
-def drp_experiment_params_builder(domain_instance_experiment):
-    experiment_params = domain_instance_experiment.drp_experiment_params
-    experiment_params.optimizer_params.plan = JaxDeepReactivePolicy(domain_instance_experiment.drp_experiment_params.topology)
-    return experiment_params
-
 def slp_experiment_params_builder(domain_instance_experiment):
     experiment_params = domain_instance_experiment.slp_experiment_params
-    experiment_params.optimizer_params.plan = JaxStraightLinePlan()
     return experiment_params
 
 if __name__ == '__main__':
@@ -66,10 +54,7 @@ if __name__ == '__main__':
     args_list = []
     
     for experiment in experiments:
-        if run_drp:
-            args_list.append( (experiment, 'drp', drp_experiment_params_builder) )  
-        if run_slp:
-            args_list.append( (experiment, 'slp', slp_experiment_params_builder) )
+        args_list.append( (experiment, 'slp', slp_experiment_params_builder) )
         
     # run experiment in parallel
     run_experiment_in_parallel(perform_experiment, args_list)
